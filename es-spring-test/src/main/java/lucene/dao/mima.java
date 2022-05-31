4d089864925d044d7c2f6afa971f8b6c641919c2
huahuajun <m18518782315@163.com>
2022-05-31 09:27:47 +0800

    git fork测试2

--- es-spring-test/src/main/java/lucene/dao/mima.java
+++ es-spring-test/src/main/java/lucene/dao/mima.java
@@ -4,6 +4,8 @@ public class mima {
     private final String mima = "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";
 
     private final String hahah = "ceshi";
+
+    private final String ceshi = "ceshi";
 }
 
 
